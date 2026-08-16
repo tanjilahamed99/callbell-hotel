6774a22c1e8d9c308db2462c7023a878940d06a2
tanjilahamed99 <tanjil113355@gmail.com>
2026-08-17 00:04:16 +0600

    name changed

--- frontend/src/utils/soket.js
+++ frontend/src/utils/soket.js
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 
-// const socket = io(import.meta.env.VITE_BACKEND_URL, {
-//   transports: ["websocket"],
-//   autoConnect: true,
-// });
-const socket = io("http://localhost:5000", {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
   transports: ["websocket"],
   autoConnect: true,
 });
+// const socket = io("http://localhost:5000", {
+//   transports: ["websocket"],
+//   autoConnect: true,
+// });
 
 export default socket;
